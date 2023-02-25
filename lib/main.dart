f3113b1e1acd8874e6e95b597a348310acd42759
janupasilva2005 <janupasilva2005@gmail.com>
2023-02-25 09:31:12 +0530

    Implemented the height widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,103 @@ class Main extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => const FirstScreen(),
+      },
+      theme: ThemeData(fontFamily: 'Manrope'),
+      color: Colors.blueGrey[800],
+    );
+  }
+}
+
+// First screen
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({super.key});
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  double height = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('BMI CALCULATOR'),
+        ),
+        backgroundColor: const Color(0xff1d2136),
+      ),
+      body: Container(
+        color: const Color(0xff1d2136),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(6),
+                  ),
+                  color: Color(0xff323244),
+                ),
+                height: 200,
+                width: double.infinity,
+                margin: const EdgeInsets.only(top: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text(
+                      'HEIGHT',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 18,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          '${height.toInt()}',
+                          style: const TextStyle(
+                              fontSize: 40,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        const Text(
+                          'cm',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Slider(
+                      min: 0.0,
+                      max: 300.0,
+                      value: height,
+                      onChanged: (value) {
+                        setState(() {
+                          height = value;
+                        });
+                      },
+                      inactiveColor: Colors.white,
+                      activeColor: const Color(0xffe83d66),
+                      thumbColor: const Color(0xffe83d66),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
